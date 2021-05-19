05fb285a7fb12534346d9f1b6c679205ade1bb98
David Ström <david.strom@callistaenterprise.se>
2021-05-19 16:26:42 +0200

    Changed

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -2,5 +2,6 @@ public class TestApplication {
     public static void main(String[] args) {
 	// a comment
         System.out.println("This is a test!");
+        String s = "a new String";
     }
 }
