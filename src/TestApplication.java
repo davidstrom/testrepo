f9265be906c5e1bd5b81a5f07c35df2d42653a5f
David Ström <david.strom@callistaenterprise.se>
2021-05-20 16:18:45 +0200

    A comment

--- src/TestApplication.java
+++ src/TestApplication.java
@@ -1,5 +1,6 @@
 public class TestApplication {
     public static void main(String[] args) {
         System.out.println("This is a test!");
+        System.out.println("Something else!");
     }
 }
